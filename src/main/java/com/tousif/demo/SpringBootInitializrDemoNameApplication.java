a4c9f32aa5586c7f997bdf52c931c147feb5add3
md.tousif <md.tousif@blr.cordis>
2024-04-15 21:33:18 +0530

    Initial Commit: Created basic Empty Skeleton Spring Boot project structure

---
+++ src/main/java/com/tousif/demo/SpringBootInitializrDemoNameApplication.java
@@ -0,0 +1,13 @@
+package com.tousif.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInitializrDemoNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootInitializrDemoNameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tousif/demo/SpringBootInitializrDemoNameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tousif.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootInitializrDemoNameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
